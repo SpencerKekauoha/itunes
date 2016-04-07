08a8dbb7dd5dff2902857aacd7227c63c82cbd97
Spencer Kekauoha <spkekauoha@gmail.com>
2016-04-07 14:54:36 -0600

    angular practice itunes clone

--- js/itunesService.js
+++ js/itunesService.js
@@ -6,9 +6,37 @@ app.service('itunesService', function($http, $q){
 
   //Write a method that accepts an artist's name as the parameter, then makes a 'JSONP' http request to a url that looks like this
   //https://itunes.apple.com/search?term=' + artist + '&callback=JSON_CALLBACK'
-  //Note that in the above line, artist is the parameter being passed in. 
+  //Note that in the above line, artist is the parameter being passed in.
   //You can return the http request or you can make your own promise in order to manipulate the data before you resolve it.
 
     //Code here
-    
+
+    this.getArtist = function(artist) {
+      var deferred = $q.defer();
+      $http({
+        method: "JSONP",
+        url: 'https://itunes.apple.com/search?term=' + artist + '&callback=JSON_CALLBACK',
+      }).then(function(response) {
+        var parsedResponse = response.data.results
+        console.log(response);
+        for (var i = 0; i < parsedResponse.length; i++) {
+          // parse something
+        }
+        deferred.resolve(parsedResponse)
+      })
+      return deferred.promise;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
